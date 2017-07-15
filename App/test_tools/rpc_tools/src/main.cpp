94f8b8bac24c2c685eb4d38c81ff6dd7f486dd9d
TomekFerens <feeroo@gmail.com>
2017-07-15 19:24:58 +0200

    Rpc test tools skelethon

---
+++ App/test_tools/rpc_tools/src/main.cpp
@@ -0,0 +1,28 @@
+/*
+ * main.cpp
+ *
+ *  Created on: Jul 9, 2017
+ *      Author: tf
+ */
+#include <iostream>
+#include "CommunicationEngine.hpp"
+
+#include "stubclient.h"
+
+using namespace jsonrpc;
+using namespace std;
+
+int main()
+{
+//    MyStubClient c(new HttpClient("http://localhost:8080"));
+//    try
+//    {
+//        cout << c.sayHello("Peter Knafl") << endl;
+//        c.notifyServer();
+//    }
+//    catch (JsonRpcException e)
+//    {
+//        cerr << e.what() << endl;
+//    }
+	return 0;
+}
