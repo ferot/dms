5fe991c619594a30b03a554bb4737bb5a4282a72
TomekFerens <feeroo@gmail.com>
2017-07-22 19:44:02 +0200

    Added log with params

--- App/common/include/logger.h
+++ App/common/include/logger.h
@@ -5,7 +5,8 @@
 #define MYLOG_CATEGORY_NAME "dms"
 #endif
 
-#define LOGMSG(priority,msg) mylog_cat(MYLOG_CATEGORY_NAME,priority,msg)
+#define LOGMSG(priority, msg) mylog_cat(MYLOG_CATEGORY_NAME, priority, msg)
+#define LOGMSG_ARG(priority, msg, args) mylog_cat(MYLOG_CATEGORY_NAME, priority, msg, args)
 
 #ifndef WITHOUT_LOG4C
 #define LOG_ERROR LOG4C_PRIORITY_ERROR
