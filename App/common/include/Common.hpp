b5bd929aec13723faf6bb6847705cda1e556933e
TomekFerens <feeroo@gmail.com>
2017-08-19 19:59:57 +0200

    Number to string converting template method.

---
+++ App/common/include/Common.hpp
@@ -0,0 +1,19 @@
+/*
+ * Common.hpp
+ *
+ *  Created on: Aug 19, 2017
+ *      Author: tf
+ */
+
+#ifndef COMMON_HPP_
+#define COMMON_HPP_
+#include <string.h>
+
+template <typename T>
+static std::string numToString(T num){
+	std::stringstream ss;
+	ss << num;
+	return ss.str();
+}
+
+#endif /* COMMON_HPP_ */
