4c691c3e970f762fe752d3536774b84f04a93a04
TomekFerens <feeroo@gmail.com>
2017-08-19 20:02:07 +0200

    Refactored test app

--- App/core_component/src/main.cpp
+++ App/core_component/src/main.cpp
@@ -15,18 +15,10 @@ using namespace std;
 int main() {
 	mylog_init();
 
-    Config conf("conf.json");
-    conf.setValue(std::string("other2"), std::string("conf"), std::string("dupaaa"));
-    std::cout<<"\nconfig file: "<< conf.getValue(std::string("other2"), std::string("conf"))<<std::endl;
-
-    conf.setValue(std::string("other3"), std::string("co11nf"), std::string("du23232paaa"));
-
-    std::cout<<"\nconfig file: "<< conf.getValue(std::string("other3"), std::string("co11nf"))<<std::endl;
-
 	LOGMSG(LOG_DEBUG, "Starting core_app main!");
-	CommunicationEngine ce("dupa", 8383);
-	ce.addConnection(string("localhost"),8383,"lol");
-	ce.addConnection(string("10.0.2.15"),8383,"lol1");
+	CommunicationEngine ce("127.0.0.3", 8383);
+	ce.addConnection("localhost",8383,"lol");
+	ce.addConnection("10.0.2.15",8383,"lol1");
 	ce.printConnections();
 	CommEngineRetCode ret = ce.startServer();
 	if(ret != COMM_ENG_SUCCESS)
