cc3cb41eb16a1ce6bcec545645ea70deca1b7faf
TomekFerens <feeroo@gmail.com>
2017-10-26 10:41:33 +0200

    Refactor of rpc implementation into MQTT

--- App/comm_component/include/CommunicationEngine.hpp
+++ App/comm_component/include/CommunicationEngine.hpp
@@ -12,6 +12,9 @@
 #include <memory>
 #include <map>
 
+#include "MQTTClient.h"
+#include "MQTTAsync.h"
+
 #include "logger.h"
 #include "Config.hpp"
 
@@ -21,39 +24,50 @@ using namespace std;
  * Node Engine Return Code.
  * Represents status of operation
  */
-enum ComEnRc{
-	COMM_ENG_SUCCESS,
-	COMM_ENG_ERROR
+enum ComEnRc {
+	COMM_ENG_SUCCESS, COMM_ENG_ERROR
 };
 
 /*
- * Class responsible for Communicating with nodes
+ * Class responsible for Communicating with MQTT broker.
  */
 class CommunicationEngine {
 private:
+	MQTTClient m_client;
+	MQTTClient_connectOptions m_conn_opts;
+
+	string m_address;
+	string m_clientId;
+	string m_topic;
+	short int m_qos;
+	int m_timeout;
+	int m_port;
+
 	static CommunicationEngine* m_instance;
 
-	CommunicationEngine(string ip, int port);
+	CommunicationEngine(string address = "tcp://localhost", string client = "",
+			int port = 1883, int timeout = 10000L, int qos = 1);
 	~CommunicationEngine();
 
-	string m_server_ip;
-	int m_server_port;
 
-	int initLogger();
 	ComEnRc obtainServerIP();
 	Config* config;
 public:
-	static CommunicationEngine* getInstance(string ip = "", int port = 0);
 
-	string getServerIp();
-	int getServerPort();
+	static CommunicationEngine* getInstance(string m_address = "", int port = 1883);
 
-	ComEnRc addConnection(string ip, int port, string id = "");
-	ComEnRc startServer();
-	ComEnRc stopServer();
-	void printConnections();
-	//SEND METHODS
-	string send(string);
+	string getBrokerAddr();
+	int getPort();
+
+	inline void setTopic(std::string topic) {
+		m_topic = topic;
+	}
+
+	ComEnRc connect();
+	ComEnRc disconnect();
+	ComEnRc subscribe(string);
+	ComEnRc unsubscribe(string);
+	ComEnRc publish(string, string);
 };
 
 #endif /* COMMUNICATIONENGINE_HPP_ */
--- App/comm_component/src/CommunicationEngine.cpp
+++ App/comm_component/src/CommunicationEngine.cpp
@@ -19,85 +19,122 @@ CommunicationEngine* CommunicationEngine::m_instance = nullptr;
  */
 CommunicationEngine* CommunicationEngine::getInstance(string ip, int port) {
 	if (!m_instance) {
-		m_instance = new CommunicationEngine(ip, port);
+		m_instance = new CommunicationEngine();
 	}
 	return m_instance;
 }
 
 /*
  * Constructor of communication engine.
- * Creates fundamental objects essential for RPC communication
+ * Creates fundamental objects essential for MQTT communication
  */
-CommunicationEngine::CommunicationEngine(string ip, int port) :
-		m_server_ip(ip), m_server_port(port) {
+CommunicationEngine::CommunicationEngine(string address, string client,
+		int port, int timeout, int qos) :
+		m_address(address), m_clientId(client), m_port(port), m_timeout(
+				timeout), m_qos(qos) {
 
 	config = Config::getInstance();
 
-	if (!initLogger()) {
-		LOGMSG(LOG_DEBUG, "Logger initialized!");
-	} else {
-		printf("Logger init failed");
-	}
+	m_conn_opts = MQTTClient_connectOptions_initializer;
+	m_conn_opts.keepAliveInterval = 20;
+	m_conn_opts.cleansession = 1;
+
+	MQTTClient_create(&m_client, m_address.c_str(), m_clientId.c_str(),
+	MQTTCLIENT_PERSISTENCE_NONE, NULL);
+
+	//MQTTClient_setCallbacks(m_client, NULL, connlost, msgarrvd, delivered);
 
 }
 
 /*
- * Cleans up connections handlers as they're allocated manually
+ * Cleans up connection handler as they're allocated manually
  */
 CommunicationEngine::~CommunicationEngine() {
-
+	if (disconnect() == COMM_ENG_SUCCESS) {
+		MQTTClient_destroy(&m_client);
+	} else {
+		LOGMSG(LOG_WARN, "Couldn't destroy client!");
+	}
 }
 
 /*
- * Initializes logger instance
- */
-int CommunicationEngine::initLogger() {
-	return mylog_init();
-}
-
-/*
- * Gets ip address from config file.
+ * Gets broker's address from config file.
  */
 ComEnRc CommunicationEngine::obtainServerIP() {
 	ComEnRc ret = COMM_ENG_SUCCESS;
-	m_server_ip = config->getValue("nodes", "host");
+	m_address = config->getValue("nodes", "broker_address");
 
-	if (m_server_ip.empty()) {
-		LOGMSG(LOG_ERROR, "Cannot get addres from config file!");
+	if (m_address.empty()) {
+		LOGMSG(LOG_ERROR, "Cannot get broker address from config file!");
 		ret = COMM_ENG_ERROR;
 	}
 	return ret;
 }
 
 /*
- * Returns server ip associated http server
+ * Returns address associated with broker.
  */
-string CommunicationEngine::getServerIp() {
-	return m_server_ip;
+string CommunicationEngine::getBrokerAddr() {
+	return m_address;
 }
 
 /*
  * Returns server port associated with http server
  */
-int CommunicationEngine::getServerPort() {
-	return m_server_port;
+int CommunicationEngine::getPort() {
+	return m_port;
 }
 
-
 /*
- * Start listening server for RPC commands
+ * Start listening for messages @topic.
  */
-ComEnRc CommunicationEngine::startServer() {
+ComEnRc CommunicationEngine::subscribe(string topic) {
 	ComEnRc ret = COMM_ENG_SUCCESS;
-	LOGMSG(LOG_DEBUG, "Starting http server...");
+	if (MQTTClient_subscribe(m_client, m_topic.c_str(),
+			m_qos) != MQTTCLIENT_SUCCESS) {
+		LOGMSG_ARG(LOG_ERROR, "Couldn't subscribe to topic %s !",
+				topic.c_str());
+		ret = COMM_ENG_ERROR;
+	} else {
+		LOGMSG_ARG(LOG_DEBUG, "Subscribing to topic %s...", topic.c_str());
+	}
 	return ret;
 }
 
 /*
- * Stops listening server for RPC commands
+ * Stops listening for messages @topic
  */
-ComEnRc CommunicationEngine::stopServer() {
+ComEnRc CommunicationEngine::unsubscribe(string topic) {
 	ComEnRc ret = COMM_ENG_SUCCESS;
 	LOGMSG(LOG_DEBUG, "Stopping http server...");
 	return ret;
 }
+
+/*
+ * Connects to MQTT broker.
+ */
+ComEnRc CommunicationEngine::publish(string msg, string topic) {
+	ComEnRc ret = COMM_ENG_SUCCESS;
+	if ((MQTTClient_connect(m_client, &m_conn_opts)) != MQTTCLIENT_SUCCESS) {
+		LOGMSG_ARG(LOG_ERROR, "Couldn't connect to broker %s !",
+				m_address.c_str());
+		ret = COMM_ENG_ERROR;
+	} else {
+		LOGMSG_ARG(LOG_DEBUG, "Connecting to broker %s...", m_address.c_str());
+	}
+	return ret;
+}
+/*
+ * Disconnects client.
+ */
+ComEnRc CommunicationEngine::disconnect() {
+	ComEnRc ret = COMM_ENG_SUCCESS;
+	if (MQTTClient_disconnect(m_client, 10000) != MQTTCLIENT_SUCCESS) {
+		LOGMSG(LOG_ERROR, "Client disconnect failure!");
+		ret = COMM_ENG_ERROR;
+	} else {
+		LOGMSG(LOG_DEBUG, "client disconnected successfully...");
+	}
+	return ret;
+}
+
