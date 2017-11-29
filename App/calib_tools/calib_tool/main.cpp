01b69c910c5cd25c2d7dbfea5437bb7174d34802
TomekFerens <feeroo@gmail.com>
2017-11-29 13:40:02 +0100

    Connecting essential MQTT libs

--- App/calib_tools/calib_tool/main.cpp
+++ App/calib_tools/calib_tool/main.cpp
@@ -1,8 +1,24 @@
 #include "calibtool.h"
 #include <QApplication>
+#include "CommunicationEngine.hpp"
+#include "DispatchEngine.hpp"
+#include "CommEvent.hpp"
+#include "Config.hpp"
+
 
 int main(int argc, char *argv[])
 {
+	if (!initLogger()) {
+		LOGMSG(LOG_DEBUG, "Logger initialized!");
+	} else {
+		printf("Logger init failed");
+	}
+
+	CommunicationEngine *ce = CommunicationEngine::getInstance();
+	ce->connect();
+	std::string topicName = Config::getInstance()->getValue("MQTT", "topic_name");
+	ce->subscribe(topicName);
+
     QApplication a(argc, argv);
     CalibTool w;
     w.show();
