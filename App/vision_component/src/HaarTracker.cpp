1073ed3a07adeb1dae55a0b6ee38b918e28f19dd
TomekFerens <feeroo@gmail.com>
2017-12-10 16:44:46 +0100

    Removed additional debug comments and added comment for haar detect update.

--- App/vision_component/src/HaarTracker.cpp
+++ App/vision_component/src/HaarTracker.cpp
@@ -45,9 +45,17 @@ cv::Ptr<HaarTracker> HaarTracker::createTracker(){
     return cv::Ptr<HaarTracker>(trck);
 }
 
+/**
+ * Runs Haar detection algorithm on desired image and ROI.
+ * Applies next cascade if first fails, to ensure that object is visible.
+ * @param image - reference to frame to be checked.
+ * @param boundingBox - region of interest (ROI) to be checked.
+ * @return true on successfull check, otherwise false.
+ */
 bool HaarTracker::update(const cv::Mat& image, cv::Rect2d& boundingBox) {
 	std::vector<cv::Rect> faces;
 	cv::Mat frame_gray;
+	bool ret = false;
 
 	cv::cvtColor(image, frame_gray, CV_BGR2GRAY);
 	cv::equalizeHist(frame_gray, frame_gray);
@@ -56,17 +64,17 @@ bool HaarTracker::update(const cv::Mat& image, cv::Rect2d& boundingBox) {
 			0 | CV_HAAR_SCALE_IMAGE, cv::Size(30, 30));
 
 	if (faces.size() == 0) {
+		LOGMSG(LOG_TRACE, "[HaarTracker::update] Applying next cascade!");
+
 		m_profile_cascade.detectMultiScale(frame_gray, faces, 1.1, 2,
 				0 | CV_HAAR_SCALE_IMAGE, cv::Size(30, 30));
 
 	}
-	if(faces.size()!= 0){
-		LOGMSG(LOG_ERROR, "DETECTED FACES");
-
-	boundingBox = faces[0];
-	} else {
-		LOGMSG(LOG_ERROR, "NO FACES");
+	//Checking again, cause it may change after second cascade apply.
+	if (faces.size() != 0) {
+		boundingBox = faces[0];
+		ret = true;
 	}
 
-	return true;
+	return ret;
 }
