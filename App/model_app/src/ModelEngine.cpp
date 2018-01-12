1d2e771aa978f773596184de132467ba029d41c2
TomekFerens <feeroo@gmail.com>
2018-01-12 12:00:32 +0100

    Cleaning main - moving engines into ModelEngine's constructor

--- App/model_app/src/ModelEngine.cpp
+++ App/model_app/src/ModelEngine.cpp
@@ -8,6 +8,7 @@
 #include <opencv2/opencv.hpp>
 #include <opencv2/tracking.hpp>
 #include <opencv2/core/ocl.hpp>
+#include "CoordRcvdCmd.hpp"
 
 cv::Mat g_frame;
 
@@ -18,6 +19,19 @@ ModelEngine::~ModelEngine(){
 
 ModelEngine::ModelEngine() :
 		rythm(this) {
+	ce = CommunicationEngine::getInstance("model_app");
+	de = DispatchEngine::getInstance();
+
+	ce->connect();
+	std::string topicName = Config::getInstance()->getValue("MQTT", "topic_name");
+
+	t_commandPtr coordEvt(new CoordsRcvdCmd(this));
+	ce->subscribe(topicName, eventType::USER_EVENT, coordEvt);
+
+	CommonRC ret = de->startEventReader();
+	if (ret == CMN_RC_SUCCESS) {
+		LOGMSG(LOG_DEBUG, "started reader thread");
+	}
 
 
 	QObject::connect(&rythm, &QTimer::timeout, this,
--- App/model_app/src/main.cpp
+++ App/model_app/src/main.cpp
@@ -11,12 +11,7 @@
 #include "main.hpp"
 #include "Event.hpp"
 
-#include "CommunicationEngine.hpp"
-#include "CommEvent.hpp"
-#include "Config.hpp"
 
-#include "DispatchEngine.hpp"
-#include "CoordRcvdCmd.hpp"
 
 #include <iostream>
 #include <thread>
@@ -41,14 +36,6 @@ int main(int argc, char **argv) {
 
 	LOGMSG(LOG_DEBUG, "Starting core_app main!");
 
-	CommunicationEngine *ce = CommunicationEngine::getInstance("model_app");
-	DispatchEngine *de = DispatchEngine::getInstance();
-
-	ce->connect();
-	std::string topicName = Config::getInstance()->getValue("MQTT", "topic_name");
-
-	t_commandPtr coordEvt(new CoordsRcvdCmd());
-	ce->subscribe(topicName, eventType::USER_EVENT, coordEvt);
 
 	QApplication app(argc, argv);
 
@@ -67,15 +54,7 @@ int main(int argc, char **argv) {
 	QObject::connect(&me, &ModelEngine::sig_notifyModelWindow,
 			&mainWindow, &Window::slot_updateModelWindow);
 
-
-	CommonRC ret = de->startEventReader();
-	if (ret == CMN_RC_SUCCESS) {
-		LOGMSG(LOG_DEBUG, "started reader thread");
-	}
-
-
 	threader.start();
-
 
 	LOGMSG(LOG_DEBUG, "QT - Window show");
 	mainWindow.show();
