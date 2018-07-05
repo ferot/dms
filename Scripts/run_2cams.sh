82aac2fe7f361ab87c13164fcf63397a0e021c87
TomekFerens <feeroo@gmail.com>
2018-07-05 23:35:50 +0200

    Added suport to automatically run two cams and sigint handler

--- Scripts/run_2cams.sh
+++ Scripts/run_2cams.sh
@@ -2,10 +2,11 @@
 #Script prepares environment to run multiple system nodes instances on single machine.
 #Prepares folders (if needed), changes configuration to avoid conflicts with other instances
 #Runs applications
-set -ex
+set -x
+
+trap 'echo "killing apps..."; pkill cam_app; pkill calib_tool; pkill model_app' INT
 
 #this should be changed(if needed) or get as input param
-
 REPO_DIR="/home/tf/repos"
 REPO_BUILD_DIR=$REPO_DIR/"dms/build"
 DST_ENV_DIR="/home/tf/dms_run"
@@ -14,13 +15,23 @@ FIRST_ENV_DIR=$DST_ENV_DIR/bin
 SECOND_ENV_DIR=$DST_ENV_DIR/bin2
 
 function run_app() {
-dir_to_run=$FIRST_ENV_DIR
+run_dirs=( "$SECOND_ENV_DIR" "$FIRST_ENV_DIR" )
+INDEX=0
+for i in ${run_dirs[*]}; do
+let INDEX=${INDEX}+1
+    echo $i
+    cd $i
+    ./cam_app & > log_${INDEX}
+    pids[${INDEX}]=$!
+done
+}
 
-cd $dir_to_run
-./cam_app&
-
-cd $SECOND_ENV_DIR
-./cam_app&
+function wait_for_jobs() {
+# wait for all pids
+for pid in ${pids[*]}; do
+    echo "waiting for $pid"
+    wait $pid
+done
 }
 
 function change_config() {
@@ -32,17 +43,20 @@ sed -i 's/"client_id": 1/"client_id": 2/' $SECOND_ENV_DIR/config.ini
 function create_dirs() {
 #prepare environment
 mkdir -p $DST_ENV_DIR
+mkdir -p $FIRST_ENV_DIR
+mkdir -p $SECOND_ENV_DIR
 
-cp -rf $REPO_BUILD_DIR/bin $FIRST_ENV_DIR
-cp -rf $REPO_BUILD_DIR/bin $SECOND_ENV_DIR
+cp -rf $REPO_BUILD_DIR/bin/* $FIRST_ENV_DIR
+cp -rf $REPO_BUILD_DIR/bin/* $SECOND_ENV_DIR
 }
 
 
 create_dirs
 change_config
 run_app
-
-
+./model_app
+./calib_tool
+wait_for_jobs
 
 
 
